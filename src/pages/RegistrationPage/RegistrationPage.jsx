739e1ba641e297aa9f14949cdb9370b96e7f30cb
OnlyTrable <wrangler2005@gmail.com>
2025-11-27 19:56:28 +0100

    Нарешті закінчив сторінку реєстрації

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -4,6 +4,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import styles from "./RegistrationPage.module.css";
 import Input from "../../components/Input/Input.jsx";
+import Button from '../../components/Button/Button.jsx';
 import logo from "../../assets/icons/logo.svg";
 
 const schema = yup.object().shape({
@@ -32,8 +33,6 @@ const RegistrationPage = () => {
   });
 
   const onSubmit = (data) => {
-    // Тут буде логіка валідації та відправки даних на бекенд
-    // Валідація вже виконана автоматично!
     console.log("Form data submitted:", data);
   };
 
@@ -68,19 +67,58 @@ const RegistrationPage = () => {
           error={errors.password?.message}
           placeholder="Password"
         />
+        
+        {/* Блок strings1 */}
         <div className={styles.strings1}>
           <p className={styles.parag}>
             People who use our service may have uploaded
           </p>
           <div className={styles.contactRow}>
-            <p className={styles.contactParag}>your contact information to Ichgram.</p> 
-            <a className={styles.learnMore} href='#'>Learn More</a>
+            <p className={styles.contactParag}>
+              your contact information to Ichgram.
+            </p>
+            <a className={styles.learnMore} href="#">
+              Learn More
+            </a>
           </div>
         </div>
-        <button type="submit" className={styles.submitButton}>
-          Sign Up
-        </button>
+
+        {/* Блок strings2 - прибрано &nbsp;, пробіли контролюються CSS */}
+        <div className={styles.strings2}>
+          <p className={styles.parag}>
+            By signing up, you agree to our 
+            <wbr />
+            <a className={styles.learnMore} href="#">
+              Terms,
+            </a>
+            <a className={styles.learnMore} href="#">
+              Privacy
+              <wbr /> Policy
+            </a>
+            <span className={styles.and}>and</span>
+            <a className={styles.learnMore} href="#">
+              Cookies Policy.
+            </a>
+          </p>
+        </div>
+        <Button 
+          text="Sign Up"
+          // Передаємо type="submit"
+          type="submit" 
+        />
       </form>
+      {/* strings3 - блок "Have an account? Log in" */}
+      <div className={styles.strings3}>
+        <div className={styles.contactRow}>
+            <p className={styles.contactParag}>
+              Have an account?
+            </p>
+            &nbsp;
+            <a className={styles.learnMore} href="/">
+              Log in
+            </a>
+          </div>
+      </div>
     </div>
   );
 };
